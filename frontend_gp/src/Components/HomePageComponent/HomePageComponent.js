4bf6a262131bdce805c7b474bb4c5b9cd7de814b
Nouhaila <you@example.com>
2023-11-22 15:53:10 +0100

    nouhaila OH

--- frontend_gp/src/Components/HomePageComponent/HomePageComponent.js
+++ frontend_gp/src/Components/HomePageComponent/HomePageComponent.js
@@ -1,12 +1,22 @@
 import "./HomePageComponent.css";
 import React from "react";
 import HeaderComponent from "../HeaderComponent/HeaderComponent";
-
+import { Button } from "antd";
+import p1 from "./p1.png";
 
 function HomePageComponent() {
   return (
     <div className="home-page-component">
-    <HeaderComponent/>
+      <HeaderComponent defaultSelectedKeys={['1']}/>
+
+      <h2> Generic porfolio</h2>
+      <p> the easiest and fast way to Show off Your Work 
+           Free trial. No credit card required.</p>
+
+      <Button className="start-btn">TRY IT FOR FREE </Button>
+      <img src={p1} className="p1"/>
+
+
     </div>
   );
 };
